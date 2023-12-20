9c2f3ca513bf1b9e15907e82a93ab15b14c181b5
Gravizz <peeke0rr@gmail.com>
2023-12-20 14:25:24 +0700

    feat(button):add button style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,38 @@
-// Start coding here
+/**@jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+export function Button(props) {
+  if (props.buttonStyle === "primary") {
+    return (
+      <button
+        css={css`
+          hight: 50px;
+          width: 171.19px;
+          padding: 16px px;
+          background-color: ${"#074EE8"};
+          color: white;
+          border: none;
+          border-radius: 4px;
+        `}
+      >
+        {`${props.buttonStyle}`}
+      </button>
+    );
+  } else if (props.buttonStyle === "secondary") {
+    return (
+      <button
+        css={css`
+          hight: 50px;
+          width: 171.19px;
+          padding: 16px px;
+          background-color: ${"#07A4E8"};
+          color: white;
+          border: none;
+          border-radius: 4px;
+        `}
+      >
+        {`${props.buttonStyle}`}
+      </button>
+    );
+  }
+}
