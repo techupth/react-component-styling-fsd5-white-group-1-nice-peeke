9306c4296da1be69f7d0354c17b4bee6ddcbc0a1
Gravizz <peeke0rr@gmail.com>
2023-12-20 16:49:34 +0700

    feat(alert):add alert box

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,57 @@
 // Start coding here
+/**@jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import errorImage from "../assets/error.png";
+import warningImage from "../assets/warning.png";
+import infoImage from "../assets/info.png";
+import successImage from "../assets/success.png";
 
-function Alert() {}
+export function Alert(props) {
+  return (
+    <div
+      className={`${props.type}-alert`}
+      css={css`
+        font-family: "Kanit", sans-serif;
+        font-family: "Outfit", sans-serif;
+        font-size: 20px;
+        font-weight: 700;
+        display: flex;
+        align-items: center;
+        width: 650px;
+        height: 76px;
+        flex-shrink: 0;
+        padding: 5px;
+        margin: 5px;
+        border-radius: 10px;
+        border: 1px solid;
+        background-color: ${props.type === "error"
+          ? "#F9C8C8"
+          : props.type === "warning"
+          ? "#F9D9C8"
+          : props.type === "info"
+          ? "#F9EBC8"
+          : props.type === "success"
+          ? "#CEF7CD"
+          : "red"};
+      `}
+    >
+      <img
+        src={`${
+          props.type === "error"
+            ? errorImage
+            : props.type === "warning"
+            ? warningImage
+            : props.type === "info"
+            ? infoImage
+            : props.type === "success"
+            ? successImage
+            : undefined
+        }`}
+        css={css`
+          margin: 10px;
+        `}
+      ></img>
+      <p>This is {`${props.type}`} alert box</p>
+    </div>
+  );
+}
